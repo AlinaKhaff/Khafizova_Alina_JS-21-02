18da5f53fb3bdb0cb8257b604c2c2ab4f3e14955
AlinaKhaff <alinakhaff@gmail.com>
2021-11-30 20:52:48 +0300

    дз16

---
+++ homework/01_firstpage/homework16/src/constants/api/dummyapi.ts
@@ -0,0 +1,16 @@
+interface IFieldsHeadApi {
+  [key: string]: string;
+}
+
+const BASE_URL: string = 'https://dummyapi.io/data/v1/';
+const USER_POINT_API: string = `${BASE_URL}user`;
+
+const API_KEY: string = '6190af67ee71f7cb9f28a4f6';
+
+const FIELDS_HEAD_API: IFieldsHeadApi = {
+  API_KEY: 'app-id'
+};
+
+export {
+  BASE_URL, USER_POINT_API, API_KEY, FIELDS_HEAD_API
+};
